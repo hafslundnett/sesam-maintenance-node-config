de8205ce5390f317146415ac4672bffc05d63381
gabriellsesam <gabriell.vig@sesam.io>
2020-06-23 15:00:36 +0200

    Add git hooks

---
+++ scripts/setup_git_hooks.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run this script to add repo git hooks to your local git config
+
+git_root=$(git rev-parse --show-toplevel)
+git config core.hooksPath $git_root/scripts/git_hooks
+cp $git_root/scripts/git_hooks/* $git_root/.git/hooks/
